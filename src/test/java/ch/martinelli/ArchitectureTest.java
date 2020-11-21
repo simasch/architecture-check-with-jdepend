25d32b7350d18a20a46d87b0bec7e13fa0a09adb
Simon Martinelli <simon@72.services>
2020-11-21 10:11:23 +0100

    Dependency Version update

--- src/test/java/ch/martinelli/ArchitectureTest.java
+++ src/test/java/ch/martinelli/ArchitectureTest.java
@@ -2,21 +2,20 @@ package ch.martinelli;
 
 import jdepend.framework.DependencyConstraint;
 import jdepend.framework.JDepend;
-import jdepend.framework.JavaClass;
 import jdepend.framework.JavaPackage;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.util.Collection;
 
-import static org.junit.Assert.assertEquals;
 
 public class ArchitectureTest {
 
     private static JDepend jdepend = new JDepend();
 
-    @BeforeClass
+    @BeforeAll
     public static void beforeClass() throws IOException {
         jdepend.addDirectory("C:\\Users\\simon\\Workspace\\architecture-check-with-jdepend\\target\\classes");
     }
@@ -48,13 +47,13 @@ public class ArchitectureTest {
 
         jdepend.analyze();
 
-        assertEquals("Architecture violation", true, jdepend.dependencyMatch(constraint));
+        Assertions.assertTrue(jdepend.dependencyMatch(constraint));
     }
 
     @Test
     public void checkCycles() {
         Collection packages = jdepend.analyze();
 
-        assertEquals("Cycles exist", false, jdepend.containsCycles());
+        Assertions.assertFalse(jdepend.containsCycles());
     }
 }
